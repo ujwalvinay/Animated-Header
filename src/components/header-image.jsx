a473c3dd42733aac4bd8f4ab37dcd0c00da4c01d
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-07-30 11:02:26 +0530

    Header images added

---
+++ src/components/header-image.jsx
@@ -0,0 +1,14 @@
+
+export default function HeaderImage()
+{
+    return (
+        <>
+            <div className="header-image">
+                <img src="../float1.svg" alt="" className="floating float1"/>
+                <img src="../float2.svg" alt="" className="floating float2"/>
+                <img src="../float3.svg" className="floating float3" alt=""/>
+                <img src="../girl.svg" className="header-img" alt=""/>
+            </div>
+        </>
+    )
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
 import HeaderText from "./headerText";
-
+import HeaderImage from "./header-image";
 export default function Header()
 {
     return (
         <>
             <div className="header">
                 <HeaderText />
+                <HeaderImage />
             </div>
         </>
     )
